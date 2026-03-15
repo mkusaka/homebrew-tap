4d14610201b70ebda986aece27eae7417bbf2248
mkusaka <hinoshita1992@gmail.com>
2026-03-15 23:17:18 +0900

    feat: add keypunch cask formula
    Initial cask formula for Keypunch macOS app.
    SHA256 is a placeholder until the first release is published.

---
+++ Casks/keypunch.rb
@@ -0,0 +1,15 @@
+cask "keypunch" do
+  version "0.0.1"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/mkusaka/keypunch/releases/download/v#{version}/Keypunch.zip"
+  name "Keypunch"
+  desc "macOS menu bar app that launches applications via global keyboard shortcuts"
+  homepage "https://github.com/mkusaka/keypunch"
+
+  app "Keypunch.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.mkusaka.Keypunch.plist",
+  ]
+end
