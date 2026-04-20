be898b5b074124d218261fa11afa433cfd8a7a39
mkusaka <hinoshita1992@gmail.com>
2026-04-21 01:24:05 +0900

    Update it2ag formula for app directory bundles

--- Formula/it2ag.rb
+++ Formula/it2ag.rb
@@ -3,20 +3,21 @@ class It2ag < Formula
   homepage "https://github.com/mkusaka/it2ag"
   license "GPL-2.0-or-later"
 
-on_macos do
-  if Hardware::CPU.arm?
-    url "https://github.com/mkusaka/it2ag/releases/download/v0.0.1/it2ag-0.0.1-darwin-arm64.tar.gz"
-    sha256 "c2ca0c0e91888d51924b9126086c066b250ba9ee9fd0b3f968a07677b00e21ee"
-  else
-    url "https://github.com/mkusaka/it2ag/releases/download/v0.0.1/it2ag-0.0.1-darwin-x64.tar.gz"
-    sha256 "db588b0c69a23d7722beafda6551bf6f28f7db30cb2bfd8283aa484d2195c252"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/mkusaka/it2ag/releases/download/v0.0.1/it2ag-0.0.1-darwin-arm64.tar.gz"
+      sha256 "c2ca0c0e91888d51924b9126086c066b250ba9ee9fd0b3f968a07677b00e21ee"
+    else
+      url "https://github.com/mkusaka/it2ag/releases/download/v0.0.1/it2ag-0.0.1-darwin-x64.tar.gz"
+      sha256 "db588b0c69a23d7722beafda6551bf6f28f7db30cb2bfd8283aa484d2195c252"
+    end
   end
-end
 
   version "0.0.1"
 
   def install
-    bin.install "it2ag"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"it2ag"
   end
 
   def caveats
